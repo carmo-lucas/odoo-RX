3dc92a569ee08144a5bb91ebd5c407b71424a7a1
Lucas Carmo <msclucaslima@gmail.com>
2024-05-26 18:58:41 -0300

    no idea what i did

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,2 @@
 from . import prescriptions
-from . import product_template
 from . import pharmaceutical_form
-from . import regulatory_classification
--- models/regulatory_classification.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class RxRegulatoryClassification(models.Model):
-    _name="rx.regulatory.classification"
-    _description="Regulatory Organizations"
-
-    name = fields.Char(string="Name", required=True, size = 64)
-    abbreviation = fields.Char(string="Abbreviation", required=True, size=12)
